2217d86d5abf1d9376f861a82101ffe04a8708f0
Ian Chiu <nk9710632@gmail.com>
2024-08-02 17:52:23 +0800

    feat: use type orm

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UserModule } from "../user/user.module";
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "user",
+            password: "password",
+            database: "task_management",
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            synchronize: true,
+            logging: true,
+        }),
+        UserModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
