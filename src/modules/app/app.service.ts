40373df2c137968212b0f216b74f4bb76ff383b2
Ian Chiu <nk9710632@gmail.com>
2024-08-02 14:04:42 +0800

    chore: update prettier config to fit my coding style

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    healthCheck(): string {
+        return "Server is up and running!";
+    }
 }
