f8004094044c273a8fa9fa5215573c4671710025
Ian Chiu <nk9710632@gmail.com>
2024-08-05 17:57:44 +0800

    feat: define response interface

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -6,10 +6,14 @@ import {
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { IResponse } from "../interfaces";
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {// TODO: Add return type
+    intercept(
+        context: ExecutionContext,
+        next: CallHandler,
+    ): Observable<IResponse> {
         return next.handle().pipe(
             map((data) => {
                 return {
---
+++ src/common/interfaces/exception.interface.ts
@@ -0,0 +1,5 @@
+export interface IExceptionResponse {
+    statusCode: number;
+    message: string;
+    error: string;
+}
