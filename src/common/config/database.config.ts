d214adeaa9ca5d655a2140d14a19f4ea56b297e7
Ian Chiu <nk9710632@gmail.com>
2024-08-09 16:38:11 +0800

    refactor: put env var to private variables

--- src/common/config/database.config.ts
+++ src/common/config/database.config.ts
@@ -4,18 +4,30 @@ import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 
 @Injectable()
 export class DatabaseConfig implements TypeOrmOptionsFactory {
-    constructor(private readonly configService: ConfigService) {}
+    #host: string;
+    #port: number;
+    #username: string;
+    #password: string;
+    #database: string;
+
+    constructor(private readonly configService: ConfigService) {
+        this.#host = this.configService.get<string>("DATABASE_HOST");
+        this.#port = this.configService.get<number>("DATABASE_PORT");
+        this.#username = this.configService.get<string>("DATABASE_USERNAME");
+        this.#password = this.configService.get<string>("DATABASE_PASSWORD");
+        this.#database = this.configService.get<string>("DATABASE_NAME");
+    }
 
     createTypeOrmOptions():
         | Promise<TypeOrmModuleOptions>
         | TypeOrmModuleOptions {
         return {
             type: "postgres",
-            host: this.configService.get<string>("DATABASE_HOST"),
-            port: this.configService.get<number>("DATABASE_PORT"),
-            username: this.configService.get<string>("DATABASE_USERNAME"),
-            password: this.configService.get<string>("DATABASE_PASSWORD"),
-            database: this.configService.get<string>("DATABASE_NAME"),
+            host: this.#host,
+            port: this.#port,
+            username: this.#username,
+            password: this.#password,
+            database: this.#database,
             entities: ["dist/**/*.entity{.ts,.js}"],
             synchronize: true,
             logging: true,
