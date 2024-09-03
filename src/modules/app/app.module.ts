c327058d326f77d691049cb1f0fcbf3db8621a74
Ian Chiu <nk9710632@gmail.com>
2024-09-03 22:09:03 +0800

    feat: add task module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { DatabaseConfig } from "../../common/config";
+import { TaskModule } from "../task/task.module";
 import { UserModule } from "../user/user.module";
 
 import { AppController } from "./app.controller";
@@ -13,6 +14,7 @@ import { AppService } from "./app.service";
         ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
         UserModule,
+        TaskModule,
     ],
     controllers: [AppController],
     providers: [AppService],
