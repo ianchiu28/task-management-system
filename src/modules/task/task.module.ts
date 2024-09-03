a2d04479cbbb22f03a5c3d67874416f3c2f8bba4
Ian Chiu <nk9710632@gmail.com>
2024-09-03 22:31:31 +0800

    feat: add user repository and user entity in task module

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -3,6 +3,8 @@ import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { JwtConfig } from "../../common/config/jwt.config";
+import { UserEntity } from "../user/user.entity";
+import { UserRepository } from "../user/user.repository";
 
 import { TaskController } from "./task.controller";
 import { TaskEntity } from "./task.entity";
@@ -12,9 +14,9 @@ import { TaskService } from "./task.service";
 @Module({
     imports: [
         JwtModule.registerAsync({ useClass: JwtConfig }),
-        TypeOrmModule.forFeature([TaskEntity]),
+        TypeOrmModule.forFeature([TaskEntity, UserEntity]),
     ],
     controllers: [TaskController],
-    providers: [TaskService, TaskRepository],
+    providers: [TaskService, TaskRepository, UserRepository],
 })
 export class TaskModule {}
