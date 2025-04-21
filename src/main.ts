9e373857d4655bd00823cfd8cc9a23050657eb19
Ian Chiu <nk9710632@gmail.com>
2025-04-21 17:43:37 +0800

    feat: present swagger on web

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { HttpExceptionFilter } from "./common/exceptionFilters";
 import { ResponseInterceptor } from "./common/interceptors";
@@ -7,6 +8,17 @@ import { AppModule } from "./modules/app/app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // Swagger
+    const config = new DocumentBuilder()
+        .setTitle("Task Management System API")
+        .setDescription("The Task Management System API description")
+        .setVersion("1.0")
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     app.useGlobalInterceptors(new ResponseInterceptor());
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalFilters(new HttpExceptionFilter());
